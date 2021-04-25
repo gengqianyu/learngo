877348d7c64b30bcd0d5ae06bdb001d38a041425
gengqianyu <gengqianyu@gmail.com>
2021-04-25 20:46:46 +0800

    edit iota

--- src/basic/basic.go
+++ src/basic/basic.go
@@ -65,6 +65,8 @@ func setConst() {
 
 //枚举
 // iota 是 golang 语言的常量计数器,只能在常量的表达式中使用。
+//iota 在 const关键字出现时将被重置为 0(const内部的第一行之前)，
+//const 中每新增一行常量声明将使 iota 计数一次( iota 可理解为 const 语句块中的行索引)。
 
 func enums() {
 	const (
