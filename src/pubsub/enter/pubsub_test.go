2efdf7217e2f716184bd7c8131e8fe6ddcbe3ecf
gengqianyu <gengqianyu@gmail.com>
2021-04-25 18:14:25 +0800

    edit comment

--- src/pubsub/enter/pubsub_test.go
+++ src/pubsub/enter/pubsub_test.go
@@ -43,7 +43,13 @@ func TestPublisher_Publish(t *testing.T) {
 		}
 	}()
 
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
-	t.Logf("quit(%v)\n", <-sig)
+	//创建一个信号量通道，等待一个信号发出
+	c := make(chan os.Signal, 1)
+	//SIGINT用户发送内部INTR字符：Ctrl+C触发
+	//SIGTERM程序终止信号：例如ps -ef | grep monitor.go | grep grep -v | awk '{print $2}' | xargs kill
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+	t.Logf("quit(%v)\n", <-c)
+	// 让signal包停止向c转发信号
+	// 它会取消之前使用c调用的所有Notify的效果。当Stop返回后，会保证c不再接收到任何信号
+	signal.Stop(c)
 }
--- src/tree/node.go
+++ src/tree/node.go
@@ -1,29 +1,29 @@
-/*
-值接收者vs指针接收者
-值接收者是go语言特有的
-(为什么呢，在其它语言的对象中均有，this，self指针对方法进行调用，
-go方法的定义都是定义在结构体外面，调用的时候就是用接收者去调用,
-接收者可以是指针/值均可)
-值/指针接收者均可接收值和指针
+//值接收者vs指针接收者
+//值接收者是go语言特有的
+//(为什么呢，在其它语言的对象中均有，this，self指针对方法进行调用，
+//go方法的定义都是定义在结构体外面，调用的时候就是用接收者去调用, 接收者可以是指针/值均可)
+//值/指针接收者均可接收值和指针
 
-*/
 package tree
 
 import "fmt"
 
 // 定义一个结构体 利用type,和struct关键字
+
 type Node struct {
 	Value       int
 	Left, Right *Node
 }
 
 // 为结构体定义方法(因为go所有的都是传值，所以想修改结构体Value的值，就利用结构体的指针)
+
 func (node Node) PrintValue() {
 	fmt.Println(node.Value)
 }
 
 // 和普通函数不同的是，在方法前面定义了一个方法接收者
 // 使用指针做为方法的接收者，不然因为值传递，无法修改结构体的值
+
 func (node *Node) SetValue(Value int) {
 	if node == nil {
 		fmt.Println("node is nil!")
@@ -33,6 +33,7 @@ func (node *Node) SetValue(Value int) {
 }
 
 // get max node value
+
 func (node *Node) GetMaxNode() int {
 	maxNode := 0
 	nodesChannel := node.TraverseWithChannel()
@@ -46,6 +47,7 @@ func (node *Node) GetMaxNode() int {
 }
 
 // 节点工厂
+
 func NodeFactory(Value int) *Node {
 	return &Node{Value: Value}
 }
