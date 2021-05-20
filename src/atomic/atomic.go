9052951bdf628fdcc37e194dfd3722428abaed1b
gengqianyu <gengqianyu@gmail.com>
2021-05-20 10:13:00 +0800

    edit atomic

--- src/atomic/atomic.go
+++ src/atomic/atomic.go
@@ -3,14 +3,15 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
+// 原子 int
 type atomicInt struct {
 	value int
 	lock  sync.Mutex //
 }
 
+// 自增
 func (a *atomicInt) increment() {
 	fmt.Println("safe increment")
 	//  使用锁保护代码区,使用匿名函数 defer 控制在函数体里面
@@ -22,13 +23,14 @@ func (a *atomicInt) increment() {
 	}()
 }
 
+// 获取
 func (a *atomicInt) get() int {
 	a.lock.Lock()
 	defer a.lock.Unlock()
 	return int(a.value)
 }
 
-//  使用waitGroup 就不用枷锁了
+//  使用 waitGroup 就不用枷锁了
 func (a *atomicInt) syncAtomic() {
 	var wg sync.WaitGroup
 
@@ -46,7 +48,6 @@ func (a *atomicInt) syncAtomic() {
 
 func (a *atomicInt) lockAtomic() {
 	a.increment()
-
 	go func() {
 		a.increment()
 	}()
@@ -54,9 +55,12 @@ func (a *atomicInt) lockAtomic() {
 
 func main() {
 	var a atomicInt
-	//a.syncAtomic()
-	a.lockAtomic()
-	// block一下要不 goroutine 没来的及increment 主程序就退出了 替代方案使用sync.WaitGroup
-	time.Sleep(time.Millisecond)
+	//利用 sync.WaitGroup 控制并发
+	a.syncAtomic()
+
+	//a.lockAtomic()
+	// block 一下要不 goroutine 没来的及 increment 主程序就退出了 替代方案使用 sync.WaitGroup
+	//time.Sleep(time.Millisecond)
+
 	fmt.Println(a.get())
 }
